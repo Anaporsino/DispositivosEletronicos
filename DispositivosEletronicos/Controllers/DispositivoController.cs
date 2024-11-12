50e5e16c216c4c1d4cab199362e5c47b37f91f3a
Ana Porsino <ana.porsino@aluno.senai.br>
2024-11-12 14:01:22 -0300

    Adicionar arquivos de projeto.

---
+++ DispositivosEletronicos/Controllers/DispositivoController.cs
@@ -0,0 +1,18 @@
+﻿using DispositivosEletronicos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DispositivosEletronicos.Controllers
+{
+    public class DispositivoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<DispositivoEletronico> dispositivos = new List<DispositivoEletronico>
+            {
+                new Computador { Marca = "Dell", Modelo = "Inspirion", AnoDeFabricacao = 2021, ehlaptop = true, MemoriaRam = 16, SistemaOperacional = "Windows11"},
+                new Computador { Marca = "Samsung", Modelo = "Inspirion", AnoDeFabricacao = 2021, ehlaptop = true, MemoriaRam = 16, SistemaOperacional = "Windows11"}
+            };
+            return View(dispositivos);
+        }
+    }
+}
---
+++ DispositivosEletronicos/Models/Computador.cs
@@ -0,0 +1,17 @@
+﻿namespace DispositivosEletronicos.Models
+{
+    //essa linha é respondavel por herdar o dispositivo
+    public class Computador : DispositivoEletronico
+    {
+        public bool ehlaptop { get; set; }
+
+        public int MemoriaRam { get; set; }
+
+        public string SistemaOperacional { get; set; }
+
+        public override string ExibirDetalhes()
+        {
+            return $"Marca: {Marca}, Modelo: {Modelo}, Ano: {AnoDeFabricacao}, SO: {SistemaOperacional}";
+        }
+    }
+}
